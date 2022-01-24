450ef052cc50dafbf9f23538c3ad9638cc93b682
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2022-01-24 23:42:49 +0100

    Put `amountFor` function inside `statement` function

--- src/ch1/index.js
+++ src/ch1/index.js
@@ -1,28 +1,27 @@
-function amountFor(aPerformance, play) {
-  let result = 0;
-
-  switch (play.type) {
-    case 'tragedy':
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case 'comedy':
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-  }
-
-  return result;
-}
-
 export function statement(invoice, plays) {
+  function amountFor(aPerformance, play) {
+    let result = 0;
+
+    switch (play.type) {
+      case 'tragedy':
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case 'comedy':
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return result;
+  }
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
@@ -39,7 +38,9 @@ export function statement(invoice, plays) {
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === play.type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
 
     // print line for this order
     result += `  ${play.name}: ${format(thisAmount / 100)} (${
