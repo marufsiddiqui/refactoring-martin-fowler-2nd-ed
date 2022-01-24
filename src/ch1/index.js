bf981150cf639119ff532d7759e31b3c1d344092
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2022-01-25 00:24:34 +0100

    `Inline Variable` of totalAmount

--- src/ch1/index.js
+++ src/ch1/index.js
@@ -1,5 +1,4 @@
 export function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
@@ -7,10 +6,9 @@ export function statement(invoice, plays) {
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
 
   return result;
@@ -64,11 +62,22 @@ export function statement(invoice, plays) {
   }
 
   function totalVolumeCredits() {
-    let volumeCredits = 0;
+    let result = 0;
 
     for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
+      result += volumeCreditsFor(perf);
     }
-    return volumeCredits;
+
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+
+    return result;
   }
 }
