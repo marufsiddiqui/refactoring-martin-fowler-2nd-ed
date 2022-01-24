95e67d8a80f0c91a169bff074a4c62eaa84f01bf
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2022-01-25 00:06:42 +0100

    Extract `format` function and inline usage

--- src/ch1/index.js
+++ src/ch1/index.js
@@ -1,4 +1,22 @@
 export function statement(invoice, plays) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+
+    // print line for this order
+    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+      perf.audience
+    } seats)\n`;
+    totalAmount += amountFor(perf);
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+
+  return result;
+
   function playFor(perf) {
     return plays[perf.playID];
   }
@@ -39,25 +57,11 @@ export function statement(invoice, plays) {
     return result;
   }
 
-  let totalAmount = 0;
-  let volumeCredits = 0;
-  let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
-
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
-    // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
-      perf.audience
-    } seats)\n`;
-    totalAmount += amountFor(perf);
+  function format(aNumber) {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      minimumFractionDigits: 2,
+    }).format(aNumber);
   }
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
 }
