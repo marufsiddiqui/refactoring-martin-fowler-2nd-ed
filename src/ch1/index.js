b42db8269e74d8d2890045c30b9df28ef0f2a99c
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2022-01-25 23:06:50 +0100

    Introduce intermediate variable `statementData`

--- src/ch1/index.js
+++ src/ch1/index.js
@@ -1,11 +1,15 @@
 export function statement(invoice, plays) {
-  return renderPlainText(invoice, plays);
+  const statementData = {
+    customer: invoice.customer,
+    performances: invoice.performances,
+  };
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
+function renderPlainText(data, plays) {
+  let result = `Statement for ${data.customer}\n`;
 
-  for (let perf of invoice.performances) {
+  for (let perf of data.performances) {
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
@@ -68,7 +72,7 @@ function renderPlainText(invoice, plays) {
   function totalVolumeCredits() {
     let result = 0;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
 
@@ -78,7 +82,7 @@ function renderPlainText(invoice, plays) {
   function totalAmount() {
     let result = 0;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
 
