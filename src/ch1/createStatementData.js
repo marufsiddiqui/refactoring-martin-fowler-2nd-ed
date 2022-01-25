c8cfe908dd1fef065de34a0fbf2cff937d1cdd09
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2022-01-26 00:19:00 +0100

    Move volumeCredits calculation to PerformanceCalculator

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -16,25 +16,13 @@ export function createStatementData(invoice, plays) {
     const result = { ...aPerformance };
     result.play = calculator.play;
     result.amount = calculator.amount;
-    result.volumeCredits = volumeCreditsFor(result);
+    result.volumeCredits = calculator.volumeCredits;
 
     return result;
   }
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-
-    result += Math.max(aPerformance.audience - 30, 0);
-
-    if ('comedy' === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
-    }
-
-    return result;
   }
 
   function totalVolumeCredits(data) {
@@ -71,6 +59,18 @@ class PerformanceCalculator {
         break;
       default:
         throw new Error(`unknown type: ${this.play.type}`);
+    }
+
+    return result;
+  }
+
+  get volumeCredits() {
+    let result = 0;
+
+    result += Math.max(this.performance.audience - 30, 0);
+
+    if ('comedy' === this.play.type) {
+      result += Math.floor(this.performance.audience / 5);
     }
 
     return result;
